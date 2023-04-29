a8fabc97c8f617f5b080c60638fb3e526207e518
Diablo <82063404+LucasDiabloChan@users.noreply.github.com>
2023-04-29 17:11:02 -0300

    fix alert
    alert temporarily removed

--- public/scripts/Objects.js
+++ public/scripts/Objects.js
@@ -136,8 +136,7 @@ function colision(){
 
         if((elmt_x >= sus_x || elmt_x <= tam_sus_x &&
          //  100  >=  130   ||   100  <=  160
-            sus_x >= elmt_x || sus_x <= tam_elmt_x)){
-        }
+            sus_x >= elmt_x || sus_x <= tam_elmt_x)){}
     });
 }
 
